c05a243f9fb4a76c9c9eab30f066fb830537096c
Hanna Taniguchi <hanely.menezes@gmail.com>
2022-12-12 23:20:20 -0300

    scheduling consumer

--- kafka-core-producer/src/main/java/com/course/kafka/producer/CarLocationProducer.java
+++ kafka-core-producer/src/main/java/com/course/kafka/producer/CarLocationProducer.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 @Service
